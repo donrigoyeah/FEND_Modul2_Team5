da4e8ce17bf2946f565ab549de0c47450cde499c
donrigoyeah <46565056+donrigoyeah@users.noreply.github.com>
2020-04-17 16:05:41 +0200

    second commit
    session mit eric

--- javascript/index.js
+++ javascript/index.js
@@ -3,27 +3,58 @@
 
 //fetch("/JSON_files/recipes.json")
 //  .then(function(resp) {
-//    return resp.json();
+//    console.log(resp);
+//    return resp.body;
+//      })
+//  .then(function(body){
+    //console.log(length(body))
+    //console.log(data[0])
+////    console.log(body.getReader());
 //  })
-//  .then(function(data){
-//    console.log(length(data))
-//    console.log(data[0])
-//    return data
-//  })
+
+
+  async function loadRecipe () {
+    // http://localhost:8080
+    const recipes = await fetch("/JSON_files/recipes.json")
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("HTTP error " + response.status);
+        }
+        //console.log(response)
+        return response.json();
+    })
+    .catch(function () {
+        this.dataError = true;
+    });
+
+    return recipes;
+}
+
+const recipes = loadRecipe();
+
+console.log(recipes.then(recipeData => recipeData));
+
+////console.log(data);
 
 //const first_food = data[0]
 
-const requestURL = "/JSON_files/recipes.json";
-const request = new XMLHttpRequest();
-request.open("GET", requestURL);
-request.responseType = "json";
-request.send();
+//async function loadRecipe (){
+//  const requestURL = "/JSON_files/recipes.json";
+//  const request = new XMLHttpRequest();
+//  request.open("GET", requestURL);
+//  request.responseType = "json";
+//  request.send();
 
-request.onload = function() {
-  const recipes = request.response; 
-  console.log(recipes)
-}
+//  request.onload = async function() {
+//    return await request.response; 
+    //console.log(recipes)
+//  }
 
+  //const recipes = await loadRecipe();
+//}
+//recipes.then(function(data){
+//  console.log(data);
+//})
 
 
 
@@ -34,11 +65,11 @@ const output_Dessert_otd = document.getElementById("output_Dessert_otd")
 const output_Drink_otd = document.getElementById("output_Drink_otd")
 
 
-output_Hauptspeise_otd.innerHTML = recipes.name
-output_Vorspeise_otd.innerHTML = recipes.name
-output_Dessert_otd.innerHTML = recipes.name
-output_Drink_otd.innerHTML = recipes.name
+//output_Hauptspeise_otd.innerHTML = recipes.name
+//output_Vorspeise_otd.innerHTML = recipes.name
+//output_Dessert_otd.innerHTML = recipes.name
+//output_Drink_otd.innerHTML = recipes.name
 
 
-
+window.onload
 
